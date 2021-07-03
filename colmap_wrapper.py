6d25458bfaa9774912213af6a3a5338e382f8287
thatbrguy <bharathrajn98@gmail.com>
2021-07-03 22:50:02 +0530

    cleanedd up colmap_wrapper.py, added some enhancements and added params.yaml

--- colmap_wrapper.py
+++ colmap_wrapper.py
@@ -1,82 +1,74 @@
 ## This code is a slightly modified version of 
 ## colmap_wrapper.py from https://github.com/Fyusion/LLFF
-
 import os
 import subprocess
 
-## TODO: Remove unnecessary comments and cleanup the code.
+def get_additional_args(dict_):
+    """
+    Gets additional args from the given dictionary.
+    """
+    additional_args = []
+    for key, value in dict_.items():
+        assert (type(key) is str) and (type(value) is str)
+        additional_args.extend([f"--{key}", value])
 
-# $ DATASET_PATH=/path/to/dataset
+    return additional_args
 
-# $ colmap feature_extractor \
-#    --database_path $DATASET_PATH/database.db \
-#    --image_path $DATASET_PATH/images
+def run_colmap(params):
+    """
+    Runs colmap
+    """
+    basedir = params.basedir
 
-# $ colmap exhaustive_matcher \
-#    --database_path $DATASET_PATH/database.db
+    db_path = os.path.join(basedir, params.db_name)
+    logfile_path = os.path.join(basedir, params.log_file_name)
+    img_folder_path = os.path.join(basedir, params.img_folder_name)
+    sparse_folder_path = os.path.join(basedir, params.sparse_folder_name)
 
-# $ mkdir $DATASET_PATH/sparse
+    if not os.path.exists(sparse_folder_path):
+        os.makedirs(sparse_folder_path)
 
-# $ colmap mapper \
-#     --database_path $DATASET_PATH/database.db \
-#     --image_path $DATASET_PATH/images \
-#     --output_path $DATASET_PATH/sparse
-
-# $ mkdir $DATASET_PATH/dense
-
-def run_colmap(basedir, match_type):
-    
-    logfile_name = os.path.join(basedir, 'colmap_output.txt')
-    logfile = open(logfile_name, 'w')
-    
+    ## Feature Extraction
     feature_extractor_args = [
-        'colmap', 'feature_extractor', 
-            '--database_path', os.path.join(basedir, 'database.db'), 
-            '--image_path', os.path.join(basedir, 'images'),
-            '--ImageReader.single_camera', '1',
-            # '--SiftExtraction.use_gpu', '0',
+        "colmap", "feature_extractor",
+        "--database_path", db_path, "--image_path", img_folder_path,
     ]
-    feat_output = ( subprocess.check_output(feature_extractor_args, universal_newlines=True) )
-    logfile.write(feat_output)
-    print('Features extracted')
 
+    additional_args = get_additional_args(params.feature_extractor)
+    feature_extractor_args += additional_args
+
+    ## TODO: Are paranthesis needed?
+    feat_output = (subprocess.check_output(feature_extractor_args, universal_newlines=True))
+
+    print("[*] feature_extractor has finished execution.")
+
+    ## Exhaustive Matching
     exhaustive_matcher_args = [
-        'colmap', match_type, 
-            '--database_path', os.path.join(basedir, 'database.db'), 
+        "colmap", "exhaustive_matcher", "--database_path", db_path
     ]
 
-    match_output = ( subprocess.check_output(exhaustive_matcher_args, universal_newlines=True) )
-    logfile.write(match_output)
-    print('Features matched')
-    
-    p = os.path.join(basedir, 'sparse')
-    if not os.path.exists(p):
-        os.makedirs(p)
+    ## TODO: Are paranthesis needed?
+    match_output = (subprocess.check_output(exhaustive_matcher_args, universal_newlines=True))
 
-    # mapper_args = [
-    #     'colmap', 'mapper', 
-    #         '--database_path', os.path.join(basedir, 'database.db'), 
-    #         '--image_path', os.path.join(basedir, 'images'),
-    #         '--output_path', os.path.join(basedir, 'sparse'),
-    #         '--Mapper.num_threads', '16',
-    #         '--Mapper.init_min_tri_angle', '4',
-    # ]
+    print("[*] exhaustive_matcher has finished execution.")
+
+    ## Mapping
     mapper_args = [
-        'colmap', 'mapper',
-            '--database_path', os.path.join(basedir, 'database.db'),
-            '--image_path', os.path.join(basedir, 'images'),
-            '--output_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
-            '--Mapper.num_threads', '16',
-            '--Mapper.init_min_tri_angle', '4',
-            '--Mapper.multiple_models', '0',
-            '--Mapper.extract_colors', '0',
+        "colmap", "mapper", "--database_path", db_path, 
+        "--image_path", img_folder_path, "--output_path", sparse_folder_path,
     ]
 
-    map_output = ( subprocess.check_output(mapper_args, universal_newlines=True) )
-    logfile.write(map_output)
-    logfile.close()
-    print('Sparse map created')
-    
-    print( 'Finished running COLMAP, see {} for logs'.format(logfile_name) )
+    additional_args = get_additional_args(params.mapper)
+    mapper_args += additional_args
 
+    ## TODO: Are paranthesis needed?
+    map_output = (subprocess.check_output(mapper_args, universal_newlines=True))
 
+    print("[*] mapper has finished execution.")
+
+    ## Writing to log file.
+    with open(logfile_path, "w") as logfile:
+        logfile.write(feat_output)
+        logfile.write(map_output)
+
+    print("[*] Finished writing to log file.")
