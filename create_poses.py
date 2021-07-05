10609e1cf1c7382796359b6b08c3d9fbce625b9d
thatbrguy <bharathrajn98@gmail.com>
2021-07-05 23:06:21 +0530

    add functionality for getting c2w and w2c matrices

--- create_poses.py
+++ create_poses.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from colmap_wrapper import run_colmap
 from read_write_model import read_model
 
@@ -11,10 +12,33 @@ def extract_poses(params):
     if not params.disable_run_colmap:
         run_colmap(params)
 
+    data = {}
     path = os.path.join(params.basedir, "sparse", "0")
     cameras, images, points3D = read_model(path, ext = ".bin")
-    import pdb; pdb.set_trace()  # breakpoint 0b9789bb //
 
+    ## Getting World to Camera transformation matrices.
+    w2c_mats = []
+
+    for key in images:
+        img = images[key]
+
+        # R --> Rotation matrix, Shape --> (3, 3) 
+        R = img.qvec2rotmat()
+        # T --> Translation vector, Shape --> (3,)
+        T = img.tvec
+        
+        # Creating the world to camera transformation matrix.
+        RT = np.eye(4)
+        RT[:3, :3] = R
+        RT[:3,  3] = T
+
+        w2c_mats.append(RT)
+
+    # Shape of w2c_mats and c2w_mats is (N, 4, 4)
+    w2c_mats = np.array(w2c_mats)
+    c2w_mats = np.linalg.inv(w2c_mats)
+
+    import pdb; pdb.set_trace()  # breakpoint 465458f8 //
 
 
 if __name__ == '__main__':
